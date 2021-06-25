3af5611f173df04e1ee2c4c1df30d540d6066305
NiveenALSmadi <niveensmadi33@gmail.com>
2021-06-25 22:52:38 +0300

    done

---
+++ Data-structures/trees/tests/test_trees.py
@@ -0,0 +1,69 @@
+import pytest
+from trees import __version__
+from trees.tree import Binary_Tree, Node,Binary_Search_Tree
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+#Can successfully instantiate an empty tree
+def test_instantiate():
+    tree = Binary_Tree()
+    assert tree.root == None
+
+#Can successfully instantiate a tree with a single root node
+def test_single_root():
+    tree = Binary_Search_Tree()
+    tree.add('niveen')
+    assert tree.root.value == 'niveen'
+
+#Can successfully add a left child and right child to a single root node
+def test_add_left_right():
+    bst = Binary_Search_Tree()
+    bst.add(10)
+    bst.add(8)
+    bst.add(20)
+    assert bst.root.value==10
+    assert bst.root.left.value==8
+    assert bst.root.right.value==20
+
+
+def test_BinarySearchTree():
+    bst = Binary_Search_Tree()
+    bst.add(10)
+    bst.add(12)
+    bst.add(5)
+    assert bst.pre_order() == [10,5,12]
+
+
+#Can successfully return a collection from a preorder traversal
+def test_pre_order(prepared_tree):
+    assert prepared_tree.pre_order() == ['A','B','D','E','C','F']
+
+#Can successfully return a collection from an inorder traversal
+def test_in_order(prepared_tree):
+    assert prepared_tree.in_order() == ['D', 'B', 'E', 'A', 'F', 'C']
+
+
+#Can successfully return a collection from a postorder traversal
+def test_Post_order(prepared_tree):
+    assert prepared_tree.Post_order() == ['D', 'E', 'B', 'F', 'C', 'A']
+
+
+def test_contains():
+    bst = Binary_Search_Tree()
+    bst.add(10)
+    assert bst.contains(20) == False
+    assert bst.contains(4) == False
+
+
+
+@pytest.fixture
+def prepared_tree():
+    tree = Binary_Tree()
+    tree.root = Node('A')
+    tree.root.left = Node('B')
+    tree.root.right = Node('C')
+    tree.root.left.left = Node('D')
+    tree.root.left.right = Node('E')
+    tree.root.right.left = Node('F')
+    return tree
---
+++ Data-structures/trees/trees/tree.py
@@ -0,0 +1,105 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.left=None
+        self.right=None 
+        
+class Binary_Tree:
+
+    def __init__(self): 
+       self.root=None 
+
+
+    def pre_order(self):
+        output=[]
+        if not self.root:
+            return output
+        def traversal(node):
+            output.append(node.value)
+            if node.left:
+                traversal(node.left)
+            if node.right:
+                traversal(node.right)
+
+        traversal(self.root)
+        return output
+
+
+    def in_order(self):
+        output=[]
+        if not self.root:
+            return output
+
+        def traversal(node):
+            if node.left:
+                traversal(node.left)
+            output.append(node.value)
+            if node.right:
+                traversal(node.right)
+
+        traversal(self.root)
+        return output
+
+
+
+    def Post_order(self):
+
+        output=[]
+
+        if not self.root:
+            return output
+
+        def traversal(node):
+            if node.left:
+                traversal(node.left)
+            if node.right:
+                traversal(node.right)
+            output.append(node.value)
+
+        traversal(self.root)
+        return output
+
+
+class Binary_Search_Tree(Binary_Tree):
+
+    def add(self,value):
+        if not self.root:
+            self.root = Node(value)
+            return
+
+        current_node=self.root
+        while True:
+            if Node(value).value < current_node.value:
+                if current_node.left:
+                    current_node= current_node.left
+                else:
+                  current_node.left = Node(value)
+                  return 
+            else:
+
+                if current_node.right:
+                    current_node=current_node.right
+                else:
+                  current_node.right = Node(value)
+                  return
+
+
+
+
+
+    def contains(self,value):
+        if  not self.root:
+            return "This is empty tree"
+
+        current_node=self.root  
+
+        while current_node :
+
+            if current_node.value== value:
+                return True
+
+            if current_node.value <value: 
+                current_node=current_node.right
+            else:
+                current_node=current_node.left
+        return False
